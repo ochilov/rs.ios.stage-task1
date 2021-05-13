cab4b9b28c158b620fe8780396e6526921cdd643
JohnO <>
2021-05-13 22:50:15 +0300

    feat: Exercise-4: implement numberConverter function

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,17 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+	NSMutableArray *arr = [[NSMutableArray alloc] init];
+	if (number) {
+		NSInteger value = number.integerValue;
+		if (value < 0)
+			value = -value;
+		NSString *strValue = [NSString stringWithFormat:@"%li", value];
+		for (NSUInteger i = strValue.length; i > 0; ) {
+			[arr addObject:[strValue substringWithRange:NSMakeRange(--i, 1)]];
+		}
+	}
+	return [arr copy];
 }
 
 @end
