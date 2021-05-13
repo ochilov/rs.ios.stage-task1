f9ae525845eb22de12e2902ef1370a6e1f9d1cf4
JohnO <>
2021-05-13 16:49:02 +0300

    feat: Exercise-2: implement ValidNumber function

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,17 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+	NSUInteger length = self.length;
+	if (length <= 0 || 100 < length) {
+		return NO;
+	}
+	for (NSUInteger i = 0; i < length; i++) {
+		unichar character = [self characterAtIndex:i];
+		if (!isdigit(character)) {
+			return NO;
+		}
+	}
+	return YES;
 }
 
 @end
