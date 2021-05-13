12feb9e0ab5e0b29bafd28bca7ed6a717ec6377a
JohnO <>
2021-05-13 19:22:42 +0300

    feat: Exercise-3: implement minRotated function

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,40 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+	if (!self.count) {
+		return nil;
+	}
+	
+	NSUInteger min = 0;
+	NSUInteger max = self.count-1;
+	// in default  - minVal is first element (for not rotated case)
+	NSNumber * minVal = [self objectAtIndex:min];
+	
+	while (max > min) {
+		NSUInteger mid = (min+max)/2;
+		NSNumber * midVal = [self objectAtIndex:mid];
+		if (minVal.integerValue <= midVal.integerValue) {
+			// [min ... mid] - sorted
+			NSNumber * maxVal = [self objectAtIndex:max];
+			if (midVal.integerValue <= maxVal.integerValue) {
+				// [mid ... max] - sorted, so
+				// minVal is found
+				break;
+			}
+			
+			// [mid ... max] - not sorted, so
+			// pivot and minVal is here
+			minVal = maxVal;
+			min = mid+1;
+			
+		} else {
+			// [min ... mid] - not sorted, so
+			// pivot and minVal is here
+			minVal = midVal;
+			max = mid-1;
+		}
+	}
+	return minVal;
 }
 
 @end
