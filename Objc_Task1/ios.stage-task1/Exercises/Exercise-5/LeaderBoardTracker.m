62d274682412453b70577feb83bc1faf5ce2c5b4
JohnO <>
2021-05-15 09:32:35 +0300

    feat: Exercise-5: implement LeaderBoardTracker function

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,26 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+	if (!playerArray.count) {
+		return @[];
+	}
+	NSMutableArray *scores = [[NSMutableArray alloc] initWithCapacity:playerArray.count];
+	for (NSNumber *player in playerArray) {
+		NSInteger score = 1;
+		NSNumber *prevRank;
+		for (NSNumber *rank in rankedArray) {
+			if (prevRank != nil && prevRank.integerValue == rank.integerValue) {
+				continue;
+			}
+			if (player.integerValue >= rank.integerValue) {
+				break;
+			}
+			score++;
+			prevRank = rank;
+		}
+		[scores addObject:@(score)];
+	}
+	return [scores copy];
 }
 
 @end
